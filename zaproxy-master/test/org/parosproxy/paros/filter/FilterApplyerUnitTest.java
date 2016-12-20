09ebe0e059f52273e5953e6e90ecd43442a88132
Jasper Marien <r0450196@aarlen.cs.kotnet.kuleuven.be>
2016-12-20 18:19:41 +0100

    future tests

--- zaproxy-master/test/org/parosproxy/paros/filter/FilterApplyerUnitTest.java
+++ zaproxy-master/test/org/parosproxy/paros/filter/FilterApplyerUnitTest.java
@@ -22,6 +22,14 @@ public class FilterApplyerUnitTest {
 	private String inappropriateContent1 = "The little nigger crossed the road.";
 	private String inappropriateContent2 = "The little duck crossed the nigger.";
 	private String inappropriateContent3 = "The little niggers crossed the road.";
+	private String inappropriateContent4 = "The little n*gger crossed the road.";
+	private String inappropriateContent5 = "Trump said he likes fanta.";
+	private String inappropriateContent6 = "The little Niggers crossed the road.";
+	private String inappropriateContent7 = "We should deport all the mexicans!";
+	private String inappropriateContent8 = "He then showed me his d*ck";
+	private String inappropriateContent9 = "The little n*gger crossed the road.";
+	private String inappropriateContent10 = "mexicans deport";
+	private String inappropriateContent11 = "deported mexicans";
 	private int threshold = 10;
 	private InappropriateElement<String> inapEl1 = new InappropriateElement<String>("trump", 4, Arrays.asList("fearmongering", "mysogenist", "bigotry"));
 	private InappropriateElement<String> inapEl2 = new InappropriateElement<String>("nigger", 10, Arrays.asList("racist"));
@@ -40,10 +48,10 @@ public class FilterApplyerUnitTest {
 
 	@Test
 	public void testApplyBasicStringFilter() {
-		assertEquals(filter.applyBasicStringFilter(appropriateContent, parsedFormatFile),appropriateContent);
-		String result1 = filter.applyBasicStringFilter(inappropriateContent1, parsedFormatFile);
-		String result2 = filter.applyBasicStringFilter(inappropriateContent2, parsedFormatFile);
-		String result3 = filter.applyBasicStringFilter(inappropriateContent3, parsedFormatFile);
+		assertEquals(filter.applyBasicFilter(appropriateContent, parsedFormatFile),appropriateContent);
+		String result1 = filter.applyBasicFilter(inappropriateContent1, parsedFormatFile);
+		String result2 = filter.applyBasicFilter(inappropriateContent2, parsedFormatFile);
+		String result3 = filter.applyBasicFilter(inappropriateContent3, parsedFormatFile);
 		System.out.println(result1);
 		System.out.println(result2);
 		System.out.println(result3);
@@ -51,5 +59,91 @@ public class FilterApplyerUnitTest {
 		assertNotSame(result2, inappropriateContent2);
 		assertNotSame(result3, inappropriateContent3);
 	}
-
+	
+	//future tests, will fail now
+	
+	@Test
+	public void testApplyBasicStringFilterCapitalLetters(){
+		String result1 = filter.applyBasicFilter(inappropriateContent5, parsedFormatFile);
+		String result2 = filter.applyBasicFilter(inappropriateContent6, parsedFormatFile);
+		System.out.println(result1);
+		System.out.println(result2);
+		assertNotSame(result1, inappropriateContent5);
+		assertNotSame(result2, inappropriateContent6);
+	}
+	
+	@Test
+	public void testApplyBasicStringFilterWithWordsInBetween(){
+		String result1 = filter.applyBasicFilter(inappropriateContent7, parsedFormatFile);
+		System.out.println(result1);
+		assertNotSame(result1, inappropriateContent7);
+	}
+	
+	@Test 
+	public void testApplyBasicStringFilterWithChangedOrder(){
+		String result1 = filter.applyBasicFilter(inappropriateContent10, parsedFormatFile);
+		System.out.println(result1);
+		assertNotSame(result1, inappropriateContent10);
+	}
+	
+	@Test
+	public void testApplyBasicStringFilterWithConjugatedVerbs(){
+		String result1 = filter.applyBasicFilter(inappropriateContent11, parsedFormatFile);
+		System.out.println(result1);
+		assertNotSame(result1, inappropriateContent11);
+	}
+	
+	@Test 
+	public void testApplyBasicStringFilterPartialCensoring(){
+		String result1 = filter.applyBasicFilter(inappropriateContent4, parsedFormatFile);
+		String result2 = filter.applyBasicFilter(inappropriateContent8, parsedFormatFile);
+		String result3 = filter.applyBasicFilter(inappropriateContent9, parsedFormatFile);
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		assertNotSame(result1, inappropriateContent4);
+		assertNotSame(result2, inappropriateContent8);
+		assertNotSame(result3, inappropriateContent9);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
